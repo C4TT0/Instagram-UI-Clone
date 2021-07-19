92a71646c610238f17bccc8656527eda3825652a
Typh0n <75157529+Typh0n12@users.noreply.github.com>
2021-07-19 09:24:12 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Dmp from './Dmp.js';
 
 export default function Header(){
 	return(
 		<>
-			<Router>
-				<div className="headerComponent">
-					<i className="far fa-camera"></i>
-					<div className="logo" style={{    backgroundSize: "92px" , marginLeft: "30px"}}></div>
-					<Link to="/inbox"><i className="far fa-paper-plane"></i></Link>
-					<Switch>
-						<Route path="/inbox">
-							<Dmp/>
-						</Route>
-					</Switch>
-				</div>
-			</Router>
+			<div className="headerComponent">
+				<i className="far fa-camera"></i>
+				<div className="logo" style={{    backgroundSize: "92px" , marginLeft: "30px"}}></div>
+				<i className="far fa-paper-plane"></i>
+			</div>
 		</>
 	)
 }
